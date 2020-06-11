46ce922a6186be82379536aca287733172df6367
kproudler <kevinproudler@gmail.com>
2020-06-11 09:21:31 -0400

    generated shelf model/controller/view/etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_172830) do
+ActiveRecord::Schema.define(version: 2020_06_10_232635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,16 @@ ActiveRecord::Schema.define(version: 2020_06_10_172830) do
     t.integer "year", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", default: [], array: true
     t.string "author"
-    t.index ["user_id"], name: "index_books_on_user_id"
+    t.integer "shelf_id"
+  end
+
+  create_table "shelves", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", default: "My Bookshelf"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shelves_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
